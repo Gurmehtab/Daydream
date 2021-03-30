9932b5368cb2ec8b81f4263224ca3e52fb87a6bd
annaraymaker <annaraymaker@ufl.edu>
2021-03-30 17:32:25 -0400

    commit tiiiime

---
+++ client/src/views/Home/Home.js
@@ -0,0 +1,18 @@
+import './Home.css';
+
+import React from 'react';
+import logo from '../../assets/logo.svg';
+
+function Home() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Homepage
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default Home;
