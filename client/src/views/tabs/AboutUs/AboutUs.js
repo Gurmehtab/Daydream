a8501a7b18658490f599dbcf5a8ed50aeb74ddb2
Likhita Manchikanti <likhi.2201@gmail.com>
2021-04-15 17:08:52 -0400

    about us refactoring

--- client/src/views/tabs/AboutUs/AboutUs.js
+++ client/src/views/tabs/AboutUs/AboutUs.js
@@ -4,20 +4,20 @@ import React from 'react';
 
 function AboutUs() {
     return (
-<div className="App">
+        <div className="App">
             <header className="App-header-AU">
-                <h2 className="heading">
+                <h2>
                     About Us
                 </h2>
             </header>
             <body className= "App-info-AU">
-                <h2 className="heading">Who we are:</h2>
-                <a> A Gainesville, FL based publication dedicated to staging the beauty, art, literature, & culture of all people. 
+                <h2>Who we are:</h2>
+                <a>A Gainesville, FL based publication dedicated to staging the beauty, art, literature, & culture of all people. 
                     We are a collective of artists and Gainesville community members. 
                 </a>
-                <h2 className="heading">What we do:</h2>
+                <h2>What we do:</h2>
                 <a>We create and share artistic content of all kinds in a virtual magazine linked in the homepage.</a>
-                <h2 className="heading">Why we do it:</h2>
+                <h2>Why we do it:</h2>
                 <a>To give an outlet to underrepresented and marginalized communities and the art they create. We wish to foster an 
                     inclusive and unique environment that allows for the diversification of the Gainesville artistic community and beyond.
 
