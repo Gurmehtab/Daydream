fddcf1f79fa922ca0d8516f1b79d82d5b6151639
Likhita Manchikanti <likhi.2201@gmail.com>
2021-04-16 15:20:26 -0400

    finished homepage

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -11,32 +11,180 @@ function Home() {
                     <source src="/intro.mp4" />
                 </Player>
             </div>
-                
-            <br />
-            <br />
-            <br />
 
-            <div className="bottom">
-                <h2 className="heading">Featured Artwork</h2>
-                <div className="featured-items-container">
-                    <div className="featured-items" />                   
-                    <div className="featured-items" />                   
-                    <div className="featured-items" />                 
+            <br/>
+            <br/>
+            <br/>
+
+            <div className="content-white">
+                <div className="qa">
+                    <h1 className="article-heading">
+                        FROM DAYDREAM TO REALITY: 
+                        <br/>
+                        A Q&A WITH THE MAGAZINE’S CREATORS
+                    </h1>
+                    <p className="article-heading">
+                        By Hope Dean
+                    </p>
+                    <br/>
+                    <p>
+                        Meet Michelle Hsia and Hannah Diasti. The two haven’t seen each other in person since March 2020, but they still managed to put together an entire magazine from scratch in the past year.                     </p>
+                    <br/>
+                    <p>
+                        As seniors at the University of Florida, Michelle is a Nutritional Sciences and Behavioral and Cognitive Neuroscience double major. Hannah is also double majoring in Biology and Women’s Studies. Both are on track for medical school, which may make you wonder — what are they doing making a magazine? 
+                    </p>
+                    <br/>
+                    <p>
+                        Here are the answers to all your questions. <i>Welcome to DayDream Magazine.</i> 
+                    </p>
                 </div>
             </div>
-  
-            <br />
-            <br />
                 
-            <div className="featured-items-container">
-                <div className="featured-items" />                   
-                <div className="featured-items" />                   
-                <div className="featured-items" />                   
+            <br/>
+            <br/>
+            <br/>
+
+            <div className="content-white">
+                <div className="qa">
+                    <h2>
+                        Q: So how did you two first meet?
+                    </h2>
+                    <br/>
+                    <p>
+                        <b>MICHELLE:</b> Hannah and I technically met in the Marston Science Library on UF’s campus. It was back when things were lively and normal, and we were both in the spring semester of our sophomore year. I was sitting at one of the tables, and I was actually — I wouldn't say complaining, I was cathartically talking about an experience that I had recently that was stressful with one of the organizations that I was in. And Hannah happened to be in that same organization. She overheard my conversation and introduced herself. 
+                    </p>
+                    <br/>
+                    <p>
+                        <b>HANNAH:</b> I didn't know Michelle before. Even though we had pretty much taken the same classes, we had never crossed paths … But I think that wasn't really the start of our friendship, necessarily. That was just probably our first formal introduction to each other. I think the start of our friendship was that following summer, when that first point of connection in the Spring was just a way to have a sort of “ally” in this other class that we were taking. And so we reached out to each other, and started to study and then became really good friends over time.
+                    </p>
+                </div>
+                <img class=".image-container" src={'chapter-1.jpg'} width={300} height={450}></img>
             </div>
 
-            <br />
-            <br />
-            <br />
+            <br/>
+            <br/>
+            <br/>
+
+            <div className="content-white-column">
+                <h2>
+                    Q: How did the idea for DayDream Magazine begin? 
+                </h2>
+                <br/>
+                <p>
+                    <b>MICHELLE:</b> The very first conversation that we ever had about DayDream was actually after this huge event at UF called Health Career Showcase. I believe it was in February 2020, which is the month before everything officially shut down … At that time, I had been debating and thinking about this concept of having something and creating something that was going to be left behind at UF that was gonna fulfill some of the niches that I had seen as being unfulfilled during that time in my undergraduate career.
+                </p>
+            </div>
+
+            <br/>
+            <br/>
+            <br/>
+
+            <div className="content-white">
+                <img class=".image-container" src={'chapter-2.jpg'} width={773.9} height={527.9}></img>
+            </div>
+
+            <br/>
+            <br/>
+            <br/>
+
+            <div className="content-white-column">
+                <p>
+                    <b>HANNAH:</b> It was really just a conversation to pass time while we waited for friends to meet up with us. But back in December 2019, I was with some other friends, and they kind of told me about an experience that they had with another  publication. It just really showed how there's a lot of awareness for the need for diversity in these different platforms, but the approach is very different. It can often just lead to tokenization. … That just sat really unwell with me. With that group of friends, we had, like, a three-hour conversation in the middle of the day. … I wanted to somehow help, to create a platform that is actually, at its core, designed for people who don't necessarily have a place to express themselves. And in no way was I envisioning a magazine. I think that was a lot of Michelle's artistic passion really coming out. 
+                </p>
+                <br/>
+                <p>
+                    <b>MICHELLE:</b> It took, I think, many more conversations about figuring out the name, doing the groundwork and frameworking things over the course of the next few months … But I think that it was just a meeting of minds at a moment where it made sense. 
+                </p>
+            </div>
+
+            <br/>
+            <br/>
+            <br/>
+
+            <div className="content-white">
+                <div className="qa">
+                    <h2>
+                        Q: Where did the five themes from this issue come from?  
+                    </h2>
+                    <br/>
+                    <p>
+                        <b>HANNAH:</b> The reason why we came into even creating this was to make sure that there is a space for every type of life that’s lived to be expressed. We wanted to make sure that whatever we were including was something that's experienced by everybody in the entire world, regardless of any sort of identity, but is expressed differently because of those different identities that we bring to the table. So when we went through and came up with the five different chapters, we wanted to come up with things that are universal in its existence, but just manifest differently. 
+                    </p>
+                    <br/>
+                    <p>
+                        <b>MICHELLE:</b> Recently, there's been a whole societal push towards unlearning and relearning. Looking at these concepts that we've known to be true for so long, and rediscovering them for the truth of what they are. I'm sure that's a continual process. I hope that is going to continue into the future indefinitely. And so I think also that these chapters, because they're universal things that are experienced in everyone's life, are an opportunity for us to help contribute to that [unlearning and learning] as well. 
+                    </p>
+                </div>
+                <img class=".image-container" src={'chapter-3.jpg'} width={355.5} height={506.7}></img>
+            </div>
+
+            <br/>
+            <br/>
+            <br/>         
+
+            <div className="content-white">
+                <img class=".image-container" src={'chapter-4.jpg'} width={400} height={600}></img>
+                <div className="qa">
+                    <h2>
+                        Q: The entire production of this magazine was virtual. 
+                        <br/>
+                        What was that like to put together? 
+                    </h2>
+                    <br/>
+                    <p>
+                        <b>HANNAH:</b> I'm very fortunate that Michelle and I are so close and so willing and able to communicate 24/7. That's probably been one of the most important factors to the success of this magazine. It would not be possible without  very strong, constant, and effective communication. I also think that there's a lot of passion behind a lot of our team members. …  I'll say also, though, it was extremely hard. I will not romanticize the process. It was extremely difficult. Even though all these different things — communication, passion, et cetera — are so vital, it's not always at its high every day. 
+                    </p>
+                    <br/>
+                    <p>
+                        <b>MICHELLE:</b> The last year or so was not easy by any means for anyone. I think that there were a lot of obstacles that came up, both for us and for our staff, that probably wouldn't have existed in another year. … But we found so many people who are so passionate about their own identity, about what they're doing, about what they hope to execute, and they’ve done amazing work. I tell Hannah all the time — sometimes I look at the things that we've done in the past year, and I say, “I cannot believe what our team was able to execute. I literally cannot believe this happened.” It seems so fictional. It doesn’t seem like reality.
+                    </p>
+                </div>
+            </div>
+
+            <br/>
+            <br/>
+            <br/>    
+
+            <div className="content-white-column">
+                <h2>
+                    Q: What do you ultimately want people to get out of DayDream Magazine? 
+                </h2>
+                <br/>
+                <p>
+                    <b>HANNAH:</b> I'll just start with the feeling — the specific feeling of reaching out to somebody who is almost shocked that we have reached out to them to contribute to our magazine, and being able to share in their excitement, that they have the opportunity to be a part of this. That feeling is just so fulfilling in a way that I cannot even express. …  [I wish] I could preserve it and bottle it up and reference it whenever I was at a low point. It has just been the largest motivator, I think, throughout this entire production. And so I ultimately want everyone who views the DayDream Magazine experience to feel that way, to feel genuine excitement and comfort.
+                </p>
+            </div>
+
+            <br/>
+            <br/>
+            <br/>
+
+            <div className="content-white">
+                <img class=".image-container" src={'chapter-5.jpg'} width={800} height={533.3}></img>
+            </div>
+
+            <br/>
+            <br/>
+            <br/>            
+
+            <div className="content-white-column">
+                <p>
+                    <b>MICHELLE:</b> Up until my sophomore year, I did not feel connected to the city of Gainesville. I didn't recognize how many different systems there are … I would really like to have the opportunity to provide that information or some of those pipelines and those connections to the outside world. At every level, the ways in which we've been able to incorporate different community members, or different community organizations, or different businesses, all of that has been so impactful. Because I think that at the end of the day, we didn't just want to make it a UF-based publication, though we are both students. We also wanted to make sure that we extended our reach and extended our connection and extended the platform that we're creating to all these facets of the community that we know are important. 
+                </p>
+                <br/>
+                <p>
+                    <b>HANNAH:</b> Gainesville is an amazing city, and I'm very happy that DayDream is able to showcase that. 
+                </p>
+                <br/>
+                <br/>                
+                <h1 className="article-heading">
+                    WELCOME TO DAYDREAM MAGAZINE.
+                </h1>
+            </div>
+
+            <br/>
+            <br/>
+            <br/>
 
             <div className="featured-video-container">
                 <Player>
@@ -45,7 +193,7 @@ function Home() {
             </div>
 
             <br />
-            <br />
+            <br/>
             <br />
                   
         </div>
