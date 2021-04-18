0c84f3254c127aa771fff784212634faf5b94b4f
Gurmehtab <58491844+Gurmehtab@users.noreply.github.com>
2021-04-18 15:01:09 -0400

    Update Contributors.js
    Fixed bugs

--- client/src/views/tabs/Contributors/Contributors.js
+++ client/src/views/tabs/Contributors/Contributors.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import './Contributors.css';
 import ContributorList from './ContributorList'
@@ -29,12 +30,23 @@ var arrayContributors = [
     },
 ]
 
-
+var a=false;
 arrayContributors=JSON.parse(localStorage.getItem('arrayContributors'));
+
+if(localStorage.getItem('loginStatus') == null){
+    localStorage.setItem('loginStatus', JSON.stringify(a));
+  }
 
 function nullCheck(){
     if(localStorage.getItem('arrayContributors') == null){
         localStorage.setItem('arrayContributors', JSON.stringify(arrayContributors));
+    }
+}
+
+function nullCheck2(){
+    var a = false;
+    if(localStorage.getItem('loginStatus') == null){
+        localStorage.setItem('loginStatus', JSON.stringify(a));
     }
 }
 
@@ -95,8 +107,9 @@ function Contributers() {
             </header>
 
            
-            
+            {nullCheck2()}
             {JSON.parse(localStorage.getItem('loginStatus'))==true &&
+            
             <div>
             
             <button onClick={() => setButtonPopup(true)}>Add Contributor</button>
@@ -156,3 +169,4 @@ function Contributers() {
 }
 
 export default Contributers;
+
